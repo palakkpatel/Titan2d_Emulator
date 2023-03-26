c5a3a9c75903e2411012b1ba422c3abc4255f624
Palak Patel <palakpatel910@gmail.com>
2023-03-25 19:11:18 -0500

    Added Inital Network Architecture in Pytorch

--- model_training.py
+++ model_training.py
@@ -31,3 +31,25 @@ class SimData(Data.Dataset):
         sim_data = torch.from_numpy(temp_sim)
         sim_parameters = torch.from_numpy(self.parameters.iloc[idx,1:].values)
         return sim_parameters, sim_data
+    
+
+# Max Height Model to generate a 2d 300x300 grid from 6 parameters
+class MaxHeightModel(nn.Module):
+    def __init__(self, input_size):
+        super(MaxHeightModel, self).__init__()
+        self.linear1 = nn.Linear(input_size, 72)
+        self.linear2 = nn.Linear(72, 1024)
+        self.deconv1 = nn.ConvTranspose2d(1024, 512, 6, 6)
+        self.deconv2 = nn.ConvTranspose2d(512, 128, 5, 5)
+        self.deconv3 = nn.ConvTranspose2d(128, 32, 5, 5)
+        self.deconv4 = nn.ConvTranspose2d(32, 1, 2, 2)
+        
+    def forward(self, x):
+        x = nn.functional.relu(self.linear1(x))
+        x = nn.functional.relu(self.linear2(x))
+        x = x.reshape(x.shape[0],1024,1,1)
+        x = nn.functional.relu(self.deconv1(x))
+        x = nn.functional.relu(self.deconv2(x))
+        x = nn.functional.relu(self.deconv3(x))
+        x = self.deconv4(x)
+        return x
